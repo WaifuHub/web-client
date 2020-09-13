86315c69885fe97445c702e0a30bb4b5f6d67ddb
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2020-09-13 22:37:35 +0100

    Adds navigation skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import * as React from "react";
+import {connect} from "react-redux";
+import {Route, Router} from "react-router-dom";
 
-function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+import history from "./history";
+import Nav from "./components/Nav";
+import Pages from "./routes/Pages";
+import {checkAuthentication} from "./actions/current";
+import {ICurrent} from "./types";
+
+interface IProps {
+    checkAuthenticationConnect: () => void;
+    isAuthenticated: boolean | null;
 }
 
-export default App;
+const App = ({
+                 checkAuthenticationConnect,
+                 isAuthenticated
+             }: IProps) => {
+    React.useEffect(() => {
+        checkAuthenticationConnect();
+    }, []);
+
+    const app = isAuthenticated !== null ? (
+        <Router history={history}>
+            <Nav/>
+            <Route component={Pages}/>
+        </Router>
+    ) : null;
+
+    return (
+        <div className="App">
+            {app}
+        </div>
+    );
+}
+
+const mapStateToProps = (state: ICurrent) => ({
+    isAuthenticated: state.isAuthenticated
+});
+
+const mapDispatchToProps = {
+    checkAuthenticationConnect: checkAuthentication
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {applyMiddleware, compose, createStore} from "redux";
+import thunkMiddleware from "redux-thunk-recursion-detect";
+
+import "./index.css";
+import App from "./App";
+import currentReducer from "./reducers/current";
+import {ICurrent} from "./types";
+
+let composeEnhancers;
+
+if (
+    process.env.NODE_ENV !== "production" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+) {
+    composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+} else {
+    composeEnhancers = compose;
+}
+
+const store = createStore<ICurrent, any, any, any>(
+    currentReducer,
+    undefined,
+    composeEnhancers(applyMiddleware(thunkMiddleware)),
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
